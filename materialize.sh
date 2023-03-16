51940f891d6bec99fb037bebf4e41254be88f9eb
Rfqa 010 Admin Admin <rfqa+010-admin@dominodatalab.com>
2023-03-16 13:36:56 -0700

    Pushed from Domino: https://annafs8591.quality-team-sandbox.domino.tech/workspace/rfqa-010-admin/Test-DEMO?executionId=64137b6f84ae1275bc4c50e4

--- materialize.sh
+++ materialize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 echo "Starting materializing"
-cd /features/feast-snowflake
+cd /features/snowflake-feast
 CURRENT_TIME=$(date -u +"%Y-%m-%dT%H:%M:%S")
 feast materialize 2022-09-15T19:20:01 $CURRENT_TIME
 echo "Finished materializing"
