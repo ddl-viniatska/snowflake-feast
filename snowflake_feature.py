7de0926ce7f53fc45f90e548d629322cb3be2638
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2023-03-20 14:25:15 -0700

    Pushed from Domino: https://features9106.quality-team-sandbox.domino.tech/workspace/integration-test/Monday-Test-GBP?executionId=6418c702e9ecaa6d6c83aa6e

--- snowflake_feature.py
+++ snowflake_feature.py
@@ -59,6 +59,7 @@ driver_stats_fv = FeatureView(
     # during retrieval for building a training dataset or serving features
     schema=[
         Field(name="CONV_RATE", dtype=Float32,tags={"team": "driver_performance"}),
+        Field(name="CONV_RATE1", dtype=Float32,tags={"team": "driver_performance"}),
         Field(name="ACC_RATE", dtype=Float32),
         Field(name="AVG_DAILY_TRIPS", dtype=Int64,tags={"team": "eng"}),
     ],
